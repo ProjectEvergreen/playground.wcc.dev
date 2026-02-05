2787bb73beb6609915319df8ed1c9da1c3794de4
Owen Buckley <owenbuckley13@gmail.com>
2026-02-05 09:39:53 -0500

    feat: init scaffolding (#4)

--- greenwood.config.ts
+++ greenwood.config.ts
@@ -1,5 +1,10 @@
+import { replBundlerResourcePlugin } from './repl-bundler-plugin.ts';
 import type { Config } from "@greenwood/cli";
 
-const config: Config = {};
+const config: Config = {
+  plugins: [
+    replBundlerResourcePlugin()
+  ]
+};
 
 export default config;
