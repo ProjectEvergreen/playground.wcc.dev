43b01651a56dfa12ee78d37a9e82e83a49f8fd55
Owen Buckley <owenbuckley13@gmail.com>
2026-05-19 09:49:09 -0400

    feat: #25 adopt renderToString from wcc

--- greenwood-plugins/repl-bundler-plugin.ts
+++ greenwood-plugins/repl-bundler-plugin.ts
@@ -1,36 +1,53 @@
 import * as rollup from "rollup";
-// TODO: depend on these modules first party?
+// TODO: depend on these modules first party?  Does Greenwood need bumps for these too?
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import type { ResourcePlugin } from "@greenwood/cli";
 
-// function externalizeFsBuiltinRollupPlugin() {
-//   return {
-//     name: 'externalize-fs-builtin',
-//     resolveId(source: string) {
-//       if (source === 'fs') {
-//         return source;
-//       }
-//       return null;
-//     },
-//     load(id: string) {
-//       if (id === 'fs') {
-//         return `
-//           const noop = () => {};
-//           const fs = {
-//             readFileSync: noop,
-//             promises: {
-//               readFile: noop
-//             }
-//           }
+// TODO: could we use this? - https://www.npmjs.com/package/rollup-plugin-polyfill-node
+function externalizeFsBuiltinRollupPlugin() {
+  return {
+    name: "externalize-fs-builtin",
+    resolveId(source: string) {
+      if (source === "fs") {
+        return source;
+      }
+      return null;
+    },
+    load(id: string) {
+      if (id === "fs") {
+        // assumes utf-8 encoding
+        return `
+          const fs = {
+            readFileSync: (url) => {
+              console.log('fs.readFileSync ->', { url });
+              const xhr = new XMLHttpRequest();
+              xhr.open('GET', url.href, false); 
+              xhr.send(null);
 
-//           export default fs;
-//         `;
-//       }
-//       return null;
-//     },
-//   };
-// }
+              if (xhr.status === 200) {
+                console.log(xhr.responseText);
+                return xhr.responseText;
+              }
+            },
+            promises: {
+              readFile: async function(url) {
+                console.log('fs.promises.readFile ->', { url });
+                const response = await fetch(url);
+                const text = await response.text();
+                console.log({ url, text });
+                return text;
+              },
+            }
+          }
+
+          export default fs;
+        `;
+      }
+      return null;
+    },
+  };
+}
 
 class ReplBundlerResource {
   extensions: string[];
@@ -47,11 +64,7 @@ class ReplBundlerResource {
     const bundle = await rollup.rollup({
       input: url.pathname,
       treeshake: false,
-      plugins: [
-        // externalizeFsBuiltinRollupPlugin(),
-        nodeResolve(),
-        commonjs(),
-      ],
+      plugins: [externalizeFsBuiltinRollupPlugin(), nodeResolve(), commonjs()],
       onLog(level, log) {
         // silence circular dependency warnings from sucrase
         if (
@@ -65,9 +78,6 @@ class ReplBundlerResource {
     const { output } = await bundle.generate({
       format: "esm",
     });
-
-    // Create a buffer from the code string to avoid body consumption issues
-    // const codeBuffer = Buffer.from(output[0].code, 'utf-8');
 
     return new Response(output[0].code, {
       headers: {
